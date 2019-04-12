d93f234f3d9aee1a05d8bdada17215bbb5236542
raochsinai <roachsinai@qq.com>
2019-04-13 00:00:55 +0800

    Fix typo.

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ cd build
 cmake .. -DCMAKE_INSTALL_PREFIX=`kf5-config --prefix` -DQT_PLUGIN_INSTALL_DIR=`kf5-config --qt-plugins`
 make
 sudo make install
-#kquitapp krunner
+#kquitapp5 krunner
 #krunner
