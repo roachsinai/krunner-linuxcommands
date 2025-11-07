d5a36d8b4c3f21fab97d50bbb85b207fe41d1f4f
RoachZhao <roachsinai@qq.com>
2025-11-07 15:19:52 +0800

    Refactor fileExists method for brevity
    Simplified file existence check in LinuxCommandsRunner.

--- linux_commands.cpp
+++ linux_commands.cpp
@@ -84,12 +84,7 @@ void LinuxCommandsRunner::reloadConfiguration()
 
 bool LinuxCommandsRunner::fileExists(QString& path) {
     QFileInfo check_file(path);
-    // check if file exists and if yes: Is it really a file and no directory?
-    if (check_file.exists() && check_file.isFile()) {
-        return true;
-    } else {
-        return false;
-    }
+    return check_file.exists() && check_file.isFile();
 }
 
 QJsonObject LinuxCommandsRunner::readJson(QString& path)
